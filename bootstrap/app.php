55db03b0019e8bd9e57e947c685b73e9369008a0
João Coelho <xjogos@gmail.com>
2025-11-28 19:51:36 -0300

    Fix WAHA webhook registration endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Console\Commands\ClearLog;
+use App\Console\Commands\FirePlaidWebhook;
+use App\Console\Commands\InitializeCurrencies;
+use App\Console\Commands\SyncWahaWebhook;
+use App\Console\Commands\TriggerRecurringInvoiceGeneration;
+use App\Console\Commands\UpdateOverdueInvoices;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,6 +16,14 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
     )
+    ->withCommands([
+        ClearLog::class,
+        FirePlaidWebhook::class,
+        InitializeCurrencies::class,
+        SyncWahaWebhook::class,
+        TriggerRecurringInvoiceGeneration::class,
+        UpdateOverdueInvoices::class,
+    ])
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
