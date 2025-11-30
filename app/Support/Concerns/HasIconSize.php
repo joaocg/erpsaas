e866fbbd20e6583505731b298cb4224f7893da3b
João Coelho <xjogos@gmail.com>
2025-11-30 10:01:05 -0300

    Make Filament icon polyfills non-conflicting

---
+++ app/Support/Concerns/HasIconSize.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Filament\Support\Concerns;
+
+trait HasIconSize
+{
+    /*
+     * Filament v3 already includes icon size helpers inside the HasIcon trait.
+     * Keeping this trait empty avoids method collisions while satisfying
+     * packages that expect the separate concern to exist in newer versions.
+     */
+}
