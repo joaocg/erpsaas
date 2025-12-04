54cd1c2227c4ee5a5827d4909b76d951b2a0d8d6
João Coelho <jcoelho@iete.com.br>
2025-12-04 08:08:07 -0300

    fix(bug) Repeat variables

--- app/Filament/Company/Resources/Sales/PartnerClientLinkResource.php
+++ app/Filament/Company/Resources/Sales/PartnerClientLinkResource.php
@@ -17,10 +17,6 @@ class PartnerClientLinkResource extends Resource
     protected static ?string $model = PartnerClientLink::class;
     protected static ?string $tenantRelationshipName = 'partnerClientLinks';
 
-    protected static ?string $tenantRelationshipName = 'partnerClientLinks';
-
-    protected static ?string $tenantRelationshipName = 'partnerClientLinks';
-
     public static function getNavigationLabel(): string
     {
         return __('Partner Client Links');
